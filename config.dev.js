ab18fcb8fb437ff2ed08b15e4806b958c72b2e28
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2024-06-06 01:19:32 +1000

    Oauth

--- config.dev.js
+++ config.dev.js
@@ -1,4 +1,10 @@
 export default {
   fiatCurrencies: [ 'aud', 'usd' ],
-  cryptoAssets: [ 'clsk', 'btc' ]
+  cryptoAssets: [ 'clsk', 'btc' ],
+  oauth: {
+    google: {
+      redirectURI: 'http://localhost:8000/oauth.html',
+      clientId: '1089545802350-9i05osq70m8c4u7ib1gsbds9eced752d.apps.googleusercontent.com'
+    }
+  }
 };
